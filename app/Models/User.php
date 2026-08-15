2e9d43d56d686e86ef33dbc539d41b1f4715931f
Ohffs <ohffsnotnow@gmail.com>
2026-08-15 17:06:28 +0100

    Update casts on the user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -31,6 +31,8 @@ class User extends Authenticatable
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
+            'is_admin' => 'boolean',
+            'is_staff' => 'boolean',
         ];
     }
 
